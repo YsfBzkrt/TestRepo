7ba2710b40f0107572eebea6598e388c18517e5f
YsfBzkrt <157572703+YsfBzkrt@users.noreply.github.com>
2025-05-18 20:57:08 +0300

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,5 @@
+# This is a simple Python file created as part of a course project.  
+# It doesn't perform any specific tasks—just prints a message.  
+
+# Display output  
+print("New Python File")
